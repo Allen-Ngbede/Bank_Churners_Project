c8a75b160c7808b132b4578d1c596bc684342361
Allen Ngbede <allenngbede1@gmail.com>
2025-11-12 14:52:17 +0100

    EDA updated

--- Bank_Churners_SQL_Query.sql
+++ Bank_Churners_SQL_Query.sql
@@ -39,10 +39,32 @@ select *
 from [dbo].[BankChurners (1)]
 where Marital_Status = 'Married' and Education_Level = 'Graduate' and Card_Category = 'Blue';
 
---  8. Return a table of existing customers whose total transaction amount is between
---  1,500 and 2000, and have at least 15,000 credit limit.
+--  8. Return a table of existing customers whose total transaction amount is between 1,500 and 2000, and have at least 15,000 credit limit.
+select *
+from [dbo].[BankChurners (1)]
+where Total_Trans_Amt  between 1500 and 2000  
+and Credit_Limit >= 15000
+and Attrition_Flag = 'Existing Customer';
+
 --  10. What is the lowest transaction amount ever transacted
+select MIN(Total_Trans_Amt) as Lowest_Transaction_Amount
+from [dbo].[BankChurners (1)]
+
 --  11. Return the information of the customer with the highest transaction amount
+select Top 1 *
+from [dbo].[BankChurners (1)]
+order by Total_Trans_Amt DESC
+
 --  12. Show the distribution of Gender in the dataset (Grouping)
---  13. Show the distribution of Education Level in the dataset and order in ascending order
---  (grouping and ordering)
+select Gender, COUNT(*) as Count_Gender,
+  CAST(100.0 * COUNT(*) / SUM(COUNT(*)) over () as decimal (5,2)) as Percent_of_Total
+from [dbo].[BankChurners (1)]
+group by Gender
+order by Count_Gender DESC;
+
+--  13. Show the distribution of Education Level in the dataset and order in ascending order (grouping and ordering)
+select Education_Level, COUNT(*) as Count_Eductaion_Level,
+  CAST(100.0 * COUNT(*) / SUM(COUNT(*)) over () as decimal (5,2)) as Percent_of_Total
+from [dbo].[BankChurners (1)]
+group by Education_Level
+order by Count_Eductaion_Level DESC;
